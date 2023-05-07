b08b4230a98248de8acce4dae35c5e3385a4697b
fres621 <126067139+fres621@users.noreply.github.com>
2023-05-07 14:24:47 +0100

    Clean up, remove console.logs

--- plugins/SplitMessages/src/index.ts
+++ plugins/SplitMessages/src/index.ts
@@ -1,7 +1,7 @@
 import patchSendMessage from "./patches/sendMessage";
 import { constants } from "@vendetta/metro/common";
 //import Settings from "./Settings";
-let patches;
+let patches = [];
 let oldVals: any = {};
 
 export default {
@@ -10,12 +10,9 @@ export default {
         oldVals.MAX_MESSAGE_LENGTH_PREMIUM = constants.MAX_MESSAGE_LENGTH_PREMIUM;
         constants.MAX_MESSAGE_LENGTH = 10000;
         constants.MAX_MESSAGE_LENGTH_PREMIUM = 10000;
-        patches = [
-            patchSendMessage(oldVals)
-        ];
+        patches.push(patchSendMessage(oldVals));
     },
     onUnload: () => {
-        console.log(oldVals);
         if (oldVals.MAX_MESSAGE_LENGTH) constants.MAX_MESSAGE_LENGTH = oldVals.MAX_MESSAGE_LENGTH;
         if (oldVals.MAX_MESSAGE_LENGTH_PREMIUM) constants.MAX_MESSAGE_LENGTH_PREMIUM = oldVals.MAX_MESSAGE_LENGTH_PREMIUM;
         for (const unpatch of patches) {
