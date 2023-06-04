3ae4a4a743aa2333eeddea5f1d134e57565c7f03
fres621 <126067139+fres621@users.noreply.github.com>
2023-06-04 12:00:04 +0100

    going insane

--- plugins/testdontuse/src/Settings.tsx
+++ plugins/testdontuse/src/Settings.tsx
@@ -8,11 +8,11 @@ export default async () => {
     try {
         const response = await fetch('http://192.168.0.22:3621/file');
         if (!response.ok) {
-            return <FormText>Error fetching the file 1</FormText> 
+            return (<FormText>Error fetching the file 1</FormText>)
         }
         const data = await response.text();
         return eval(data);
       } catch (error) {
-        return <FormText>Error fetching the file 2</FormText> 
+        return (<FormText>Error fetching the file 2</FormText>) 
       }
 }
