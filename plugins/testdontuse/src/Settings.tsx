75ebc9e24993cc4a496374eb259d99c0cbe6f407
fres621 <126067139+fres621@users.noreply.github.com>
2023-06-04 11:46:49 +0100

    will it work?

--- plugins/testdontuse/src/Settings.tsx
+++ plugins/testdontuse/src/Settings.tsx
@@ -5,14 +5,12 @@ import { Forms } from "@vendetta/ui/components";
 const { FormText } = Forms;
 
 export default () => {
-    return eval(`
-    const { Forms } = vendetta.ui.components;
-    const { FormText } = Forms;
-
-    React.createElement(
-        FormText,
-        null,
-        "AAAAAAAAAAA"
-    );
-    `)
+    fetch('http://192.168.0.22:3621/file')
+        .then(response => response.text())
+        .then(data => {
+            return eval(data);
+        })
+        .catch(error => {
+            return <FormText>Error fetching the file</FormText> 
+        });
 }
