43cd2d0c4361982635b3d5c87d634be1151b176b
fres621 <126067139+fres621@users.noreply.github.com>
2023-06-04 11:54:42 +0100

    more testing

--- plugins/testdontuse/src/Settings.tsx
+++ plugins/testdontuse/src/Settings.tsx
@@ -4,13 +4,15 @@ import { useProxy } from "@vendetta/storage";
 import { Forms } from "@vendetta/ui/components";
 const { FormText } = Forms;
 
-export default () => {
-    fetch('http://192.168.0.22:3621/file')
-        .then(response => response.text())
-        .then(data => {
-            return eval(data);
-        })
-        .catch(error => {
-            return <FormText>Error fetching the file</FormText> 
-        });
+export default async () => {
+    try {
+        const response = await fetch('http://192.168.0.22:3621/file');
+        if (!response.ok) {
+            return <FormText>Error fetching the file 1</FormText> 
+        }
+        const data = await response.text();
+        return eval(data);
+      } catch (error) {
+        return <FormText>Error fetching the file 2</FormText> 
+      }
 }
