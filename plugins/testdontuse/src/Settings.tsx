9d9c62e3cb948022ba2cafd4147bb609964f5044
fres621 <126067139+fres621@users.noreply.github.com>
2023-06-04 10:35:34 +0100

    testing

--- plugins/testdontuse/src/Settings.tsx
+++ plugins/testdontuse/src/Settings.tsx
@@ -1,6 +1,9 @@
+import { storage } from "@vendetta/plugin";
+import { useProxy } from "@vendetta/storage";
+
 import { Forms } from "@vendetta/ui/components";
 const { FormText } = Forms;
 
 export default () => (
-    console.log("a")
+    storage.code ? eval(storage.code) : console.log("No code found in plugin's storage.")
 )
