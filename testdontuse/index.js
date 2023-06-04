341271fbf87bd16beacc2497e827cc42ac850fd4
fres621 <fres621@users.noreply.github.com>
2023-06-04 10:47:12 +0000

    deploy: 75ebc9e24993cc4a496374eb259d99c0cbe6f407

--- testdontuse/index.js
+++ testdontuse/index.js
@@ -1,10 +1,1 @@
-(function(exports){"use strict";function n(){return eval(`
-    const { Forms } = vendetta.ui.components;
-    const { FormText } = Forms;
-
-    React.createElement(
-        FormText,
-        null,
-        "AAAAAAAAAAA"
-    );
-    `)}let o=[];var index={onLoad:function(){o.push(function(){console.log("uwu")})},onUnload:function(){for(const e of o)e()},settings:n};return exports.default=index,Object.defineProperty(exports,"__esModule",{value:!0}),exports})({});
+(function(exports,components){"use strict";const{FormText}=components.Forms;function n(){fetch("http://192.168.0.22:3621/file").then(function(t){return t.text()}).then(function(data){return eval(data)}).catch(function(t){return React.createElement(FormText,null,"Error fetching the file")})}let o=[];var index={onLoad:function(){o.push(function(){console.log("uwu")})},onUnload:function(){for(const t of o)t()},settings:n};return exports.default=index,Object.defineProperty(exports,"__esModule",{value:!0}),exports})({},vendetta.ui.components);
