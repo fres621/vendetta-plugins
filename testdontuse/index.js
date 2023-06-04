a30c83e676b0ca613b22e188b19e9f405c338299
fres621 <fres621@users.noreply.github.com>
2023-06-04 11:20:25 +0000

    deploy: c4ca1784777b79f8c7afb0a145725a3809ec0aa1

--- testdontuse/index.js
+++ testdontuse/index.js
@@ -1,4 +1,4 @@
-(function(exports){"use strict";async function n(){return eval(`
+(function(exports){"use strict";function n(){return eval(`
     const { Forms } = vendetta.ui.components;
     const { FormText } = Forms;
     
@@ -6,5 +6,5 @@
         FormText,
         null,
         "hjdss"
-    )
+    );
     `)}let o=[];var index={onLoad:function(){o.push(function(){console.log("uwu")})},onUnload:function(){for(const e of o)e()},settings:n};return exports.default=index,Object.defineProperty(exports,"__esModule",{value:!0}),exports})({});
