0d36cf06c3bd5ebde73828cfe7a49e257adc35b0
fres621 <fres621@users.noreply.github.com>
2023-06-04 10:27:43 +0000

    deploy: f725d297bc68c9620e8ad4619425d57b93fc13cd

--- testdontuse/index.js
+++ testdontuse/index.js
@@ -1,1 +1,7 @@
-(function(n){"use strict";function t(){}let o=[];var u={onLoad:function(){o.push(function(){console.log("uwu")})},onUnload:function(){for(const e of o)e()},settings:t};return n.default=u,Object.defineProperty(n,"__esModule",{value:!0}),n})({});
+(function(exports){"use strict";function n(){return eval(`
+    React.createElement(
+        FormText,
+        null,
+        "AAAAAAAAAAA"
+    );
+    `)}let o=[];var index={onLoad:function(){o.push(function(){console.log("uwu")})},onUnload:function(){for(const e of o)e()},settings:n};return exports.default=index,Object.defineProperty(exports,"__esModule",{value:!0}),exports})({});
