5cabb22f0553640297479a0de41ec84634231905
fres621 <126067139+fres621@users.noreply.github.com>
2023-06-14 23:51:22 +0100

    oops forgot to call the patch function

--- plugins/RoleColorEverywhere/src/index.ts
+++ plugins/RoleColorEverywhere/src/index.ts
@@ -1,5 +1,6 @@
 import patchTypingWrapper from "./patches/TypingWrapper";
 import patchDCDChatManager from "./patches/DCDChatManager";
+import patchVoiceUserConnected from "./patches/VoiceUserConnected";
 import Settings from "./Settings";
 
 let patches = [];
@@ -8,6 +9,7 @@ export default {
     onLoad: () => {
         patches.push(patchTypingWrapper());
         patches.push(patchDCDChatManager());
+        patches.push(patchVoiceUserConnected());
     },
     onUnload: () => {
         for (const unpatch of patches) {
