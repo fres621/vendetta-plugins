1c86a86d412da12a2803fa16ed8613df8f137579
fres621 <126067139+fres621@users.noreply.github.com>
2023-06-14 11:04:18 +0100

    debugging

--- plugins/RoleColorEverywhere/src/index.ts
+++ plugins/RoleColorEverywhere/src/index.ts
@@ -1,18 +1,12 @@
 import patchTypingWrapper from "./patches/TypingWrapper";
 import patchDCDChatManager from "./patches/DCDChatManager";
 import Settings from "./Settings";
-import { storage } from '@vendetta/plugin';
-import { useProxy } from '@vendetta/storage';
+
 
 let patches = [];
 
 export default {
     onLoad: () => {
-        console.log("first log");
-        useProxy(storage);
-        console.log("second");
-        storage.chatInterpolation ??= 0;
-        console.log("third");
         patches.push(patchTypingWrapper());
         patches.push(patchDCDChatManager());
     },
