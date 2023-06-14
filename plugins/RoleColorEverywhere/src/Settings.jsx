7d9790578cdbb3d95851580be8a7e4c0368a1a7b
fres621 <126067139+fres621@users.noreply.github.com>
2023-06-14 11:02:38 +0100

    debugging

--- plugins/RoleColorEverywhere/src/Settings.jsx
+++ plugins/RoleColorEverywhere/src/Settings.jsx
@@ -5,7 +5,7 @@ import { useProxy } from "@vendetta/storage";
 import { storage } from "@vendetta/plugin";
 import { getAssetIDByName } from "@vendetta/ui/assets";
 console.log("d0");
-import Swidew from './ui/Swidew.jsx';
+
 console.log("d1");
 const ThemeStore = findByStoreName("ThemeStore");
 const { meta: { resolveSemanticColor } } = findByProps("colors", "meta");
@@ -51,7 +51,7 @@ export default () => {
                 </FormSwitchRow>
 
                 <Text style={{marginLeft: '5%', color: interpolateColor(Colors.text, "#ff0000", uwu/100), fontFamily: constants.Fonts.DISPLAY_MEDIUM, fontSize: 16, marginBottom: 5, marginTop: -5}}>Color interpolation (for chat text):</Text>
-                <Swidew onSlide={(v)=>{storage.chatInterpolation = v}} value={storage.chatInterpolation} />
+
             </FormSection>
         </ReactNative.ScrollView>
     );
