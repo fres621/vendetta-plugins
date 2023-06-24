8f839fffab8a14f46e946b0a2bc1bcab6a5ea232
fres621 <126067139+fres621@users.noreply.github.com>
2023-06-24 19:44:55 +0100

    fix

--- plugins/RoleColorEverywhere/src/patches/VoiceUserConnected.ts
+++ plugins/RoleColorEverywhere/src/patches/VoiceUserConnected.ts
@@ -9,12 +9,11 @@ const { meta: { resolveSemanticColor } } = findByProps("colors", "meta");
 export default function patchVoiceUserConnected() {
     return after("default", findByName("VoiceUserConnected", false),([args],res)=>{
         if (storage.noVoice) return;
-        if (!args?.member?.colorString) return;
         const unpatchRender = after("render", res.type.prototype, (a, res) => {
             unpatchRender();
             let text = (res.props?.children?.[1]?.props);
             if (!text) return;
-            text.style.color = args.member.colorString || resolveSemanticColor(ThemeStore.theme, semanticColors.CHANNELS_DEFAULT);
+            text.style.color = args?.member?.colorString || resolveSemanticColor(ThemeStore.theme, semanticColors.CHANNELS_DEFAULT);
         });
     });
 };
