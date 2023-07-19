76862fa9a7e50d1fde38be4fd186d786db26255e
fres621 <126067139+fres621@users.noreply.github.com>
2023-07-19 17:07:49 +0100

    better like this

--- plugins/AltMediaPicker/src/patch.ts
+++ plugins/AltMediaPicker/src/patch.ts
@@ -1,35 +1,35 @@
 import { findByDisplayName, findByProps, findByStoreName } from "@vendetta/metro";
-import { ReactNative } from "@vendetta/metro/common";
 import { before } from "@vendetta/patcher";
 
-const { NativeModules: nm } = ReactNative;
 const SelectedChannelStore = findByStoreName("SelectedChannelStore");
 
-// https://stackoverflow.com/a/58326357
-const genRanHex = size => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
-
-const FileManager = nm.DCDFileManager ?? nm.RTNFileManager;
-
 function mediapick(channelId) {
-    findByProps("openImagePicker").openImagePicker({}).then(image => {
-        if (!image) return;
-        let id = genRanHex(24)+".png";
-        FileManager.writeFile('cache', id, image.split(",")[1], 'base64').then(cacheUri => {
-            ReactNative.Image.getSize('file://'+cacheUri, ((w,h)=>{
-
-            findByProps("handleAttachFile").handleSelectKeyboardItem(channelId,  { node: 
-                { image: 
-                { playableDuration: 0,
-                    height: h,
-                    filename: id,
-                    width: w,
-                    mimeType: 'image/png',
-                    uri: 'file://'+cacheUri },
-                type: 'ALAssetTypePhoto' } }, false, {target:0}, undefined);
-
-            }));
-        });
-    });
+    findByProps("launchImageLibrary").launchImageLibrary(
+        {
+          mediaType: "photo",
+        },
+        ({ assets }) => {
+          findByProps("handleAttachFile").handleSelectKeyboardItem(
+            channelId,
+            {
+              node: {
+                image: {
+                  playableDuration: 0,
+                  height: assets[0].height,
+                  filename: assets[0].fileName,
+                  width: assets[0].width,
+                  mimeType: assets[0].type,
+                  uri: assets[0].uri,
+                },
+                type: "ALAssetTypePhoto",
+              },
+            },
+            false,
+            { target: 0 },
+            undefined
+          );
+        }
+      );
 };
 
 const Pressable = findByDisplayName("Pressable",false);
