1ace271bb356fa4bbb051a9a4942b70b33f2a81d
fres621 <126067139+fres621@users.noreply.github.com>
2023-08-23 07:10:09 +0100

    test

--- plugins/FileContentPreview/patches/Chat.tsx
+++ plugins/FileContentPreview/patches/Chat.tsx
@@ -1,13 +1,13 @@
 import { findByProps, findByName, findByStoreName } from "@vendetta/metro";
 import { after, before } from "@vendetta/patcher";
 import { getAssetIDByName } from "@vendetta/ui/assets";
-import React from "react";
+import { React, ReactNative } from "@vendetta/metro/common";
 import { Forms, General } from "@vendetta/ui/components";
 
-const { Text, TouchableOpacity } = General;
-const { FormIcon } = Forms;
+const { View, TouchableOpacity } = General;
+const { FormIcon, FormText } = Forms;
+const { ActivityIndicator } = ReactNative;
 
-const WebView = findByName("WebView");
 const { default: Navigator, getRenderCloseButton } = findByProps('getRenderCloseButton');
 const modals = findByProps('pushModal');
 
@@ -24,74 +24,24 @@ function testBtn(onPress) {
     );
 };
 
-let maxBytes = 1024*100; // 1 kb
-
-function createFCModal(filename = "unknown", url = "https://cdn.discordapp.com/attachments/593084706593964043/1143675462376239236/SPOILER_a.txt", bytes = 1*1024*1024) {
+function createFCModal(filename = "unknown", url = "https://cdn.discordapp.com/attachments/593084706593964043/1143675462376239236/SPOILER_a.txt", bytes = 1) {
     return ()=>{
-        const [count, setCount] = React.useState("");
-        let injectedJS = `
-        let url = "${url}";
-        let maxBytes = 1;
-        (async () => {
-            try {
-                const response = await fetch(url);
-
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-
-                const reader = response.body.getReader();
-                let bytesRead = 0;
-                let chunks = [];
-                while (bytesRead < maxBytes) {
-                    const {
-                        done,
-                        value
-                    } = await reader.read();
-                    if (done) {
-                        break;
-                    }
-                    chunks.push(value);
-                    bytesRead += value.length;
-                }
-                reader.cancel("Download size limit reached.");
-
-                const result = new Uint8Array(bytesRead);
-                let offset = 0;
-
-                for (const chunk of chunks) {
-                    result.set(chunk, offset);
-                    offset += chunk.length;
-                }
-
-                let str = (new TextDecoder().decode(result));
-                window.ReactNativeWebView.postMessage(str);
-            } catch (error) {
-                window.ReactNativeWebView.postMessage("Error reading file content: " + error.message);
-            }
-        })();
-        `;
-        let wv = <WebView 
-            source={{uri: 'https://cdn.discordapp.com/robots.txt'}}
-            style={{display: 'none'}}
-            injectedJavaScript={injectedJS}
-            onMessage={(event) => {
-                setCount(event.nativeEvent.data);
-            }} />;
-
-        let rnfetch = (bytes > maxBytes);
+        const [content, setContent] = React.useState("");
+        let maxBytes = '100';
         
-        if (!count && rnfetch) {
-            fetch(url).then(r=>{
+        fetch(url, {
+            headers: {
+              'Range': 'bytes=0-' + maxBytes
+            }
+        }).then(r=>{
             if (!r.ok) {
-                setCount("Error reading file content: Network response was not ok");
+                setContent("Error reading file content: Network response was not ok");
             } else {
                 r.text().then(text=>{
-                setCount(text);
+                    setContent(text);
                 });
             };
-            });
-        };
+        });
         
         
         return (
@@ -103,10 +53,15 @@ function createFCModal(filename = "unknown", url = "https://cdn.discordapp.com/a
                     headerRight: testBtn(() => { console.log("uwu") }),
                     render: () => {
                         return (
-                        <>
-                            <Text>{count ? count : "Loading file content..."}</Text>
-                            {rnfetch ? null : wv}
-                        </>
+                        content ? (  
+                          <View style={{margin: 32}}>
+                            <ActivityIndicator size="large" />
+                          </View>
+                          ) : (    
+                          <View style={{margin: 15}}>
+                            <FormText>{content}</FormText>
+                          </View>
+                          )
                         );
                     },
                     title: filename.toUpperCase()
