c6ffe3732ce18319827eb963160ce61926659243
fres621 <126067139+fres621@users.noreply.github.com>
2023-09-01 14:16:57 +0100

    i actually think now

--- plugins/FileContentPreview/Settings.jsx
+++ plugins/FileContentPreview/Settings.jsx
@@ -17,14 +17,26 @@ const Colors = {
     text: resolveSemanticColor(ThemeStore.theme, semanticColors.TEXT_NORMAL)
 };
 
+const humanize = findByProps("intword");
+
 export default () => {
     useProxy(storage);
     storage.chunkSize ??= 2569728;
+    let ptBytes = (v)=>(v / 100) * (51200000 - 10240) + 10240;
+    let unpt = (ptBytes) => ((ptBytes - 10240) / (51200000 - 10240)) * 100;
+    let hm = (v)=> humanize.intword(v, [ 'bytes', 'KB', 'MB', 'GB', 'TB', 'PB' ], 1024, undefined, undefined, undefined, ' ');
     return (
         <ScrollView style={{flex: 1, marginTop: 10}}>
             <FormRow label="Load chunk size" subLabel="How many bytes to load at a time"></FormRow>
-            <Swidew onSlide={(v)=>{setAsd(v)}} value={asd} style={{marginBottom: 0}} />
-            <Text style={{marginLeft: '5%', color: Colors.text, fontFamily: constants.Fonts.DISPLAY_MEDIUM, fontSize: 16, marginBottom: 5, marginTop: -5}}>Current value: {`${formatted} (${asd}%)`}</Text>
+            <Swidew onSlide={(v)=>{ storage.chunkSize = ptBytes(v) }} value={ unpt(storage.chunkSize) } style={{marginBottom: 0}} />
+            <Text 
+            style={{
+                marginLeft: '5%', color: Colors.text, 
+                fontFamily: constants.Fonts.DISPLAY_MEDIUM, fontSize: 16, 
+                marginBottom: 5, marginTop: -5
+                }}>
+                    Current value: {`${ hm(storage.chunkSize) } (${ unpt(storage.chunkSize) }%)`}
+            </Text>
         </ScrollView>
     )
 }
