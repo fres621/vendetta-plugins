28a34705b763b2a375591afc1d329e96d51ce018
fres621 <126067139+fres621@users.noreply.github.com>
2023-10-17 17:00:25 +0100

    my bad

---
+++ plugins/AudioPlayer/index.ts
@@ -0,0 +1,18 @@
+import patchRows from "./patches/Rows";
+import patchLinks from "./patches/Link";
+import tick from "./patches/Tick";
+
+let patches = [];
+
+export default {
+    onLoad: () => {
+        patches.push(patchRows());
+        patches.push(patchLinks());
+        patches.push(tick());
+    },
+    onUnload: () => {
+        for (const unpatch of patches) {
+            unpatch();
+        };
+    }
+}
