40f1a022cbea7b3c429ac59fb0975e76e71486ea
fres621 <126067139+fres621@users.noreply.github.com>
2023-11-26 22:25:25 +0000

    temporary way to access the pinned DMs
    i am about to shower and might sleep after cuz i always get eepy after shower... >~<

--- plugins/PinDMs/index.tsx
+++ plugins/PinDMs/index.tsx
@@ -1,16 +1,32 @@
 import { storage } from "@vendetta/plugin";
 import patch from "./patches/PrivateChannels";
 
+const PinDMsDevApi = {
+    pin: (channelId) => {
+        storage.pinnedDMs.push(channelId);
+    },
+    unpin: (channelId) => {
+        storage.pinnedDMs = storage.pinnedDMs.filter(e => e != channelId);
+    },
+    getPinnedDMs: () => storage.pinnedDMs
+};
+
 let patches = [];
 
 export default {
     onLoad: () => {
         storage.pinnedDMs ??= [];
         patches.push(patch());
+        // Temporary, proper Settings will be added later on
+        Object.defineProperty(window, "PinDMsDevApi", {
+            configurable: true,
+            value: PinDMsDevApi
+        });
     },
     onUnload: () => {
         for (const unpatch of patches) {
             unpatch();
+            delete window["PinDMsDevApi"];
         };
     }
 }
