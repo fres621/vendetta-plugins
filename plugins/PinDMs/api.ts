e5112083a6a0be76a8564c3089d8eb72ed379bd4
fres621 <126067139+fres621@users.noreply.github.com>
2023-12-05 02:40:02 +0000

    Added types to the api

--- plugins/PinDMs/api.ts
+++ plugins/PinDMs/api.ts
@@ -1,31 +1,32 @@
 import { storage } from "@vendetta/plugin";
 import { findByProps } from "@vendetta/metro";
+import { PinnedDMsCategory } from "./types";
 const { fromTimestamp } = findByProps("extractTimestamp");
 
 // TODO maybe add storage.collapsedPinnedDMs in the api
 const PinDMsApi = {
-    pin: (channelId, categoryId) => {
+    pin: (channelId: string, categoryId: string): void => {
         // v Not sure if it's a good idea to add the appropiate restrictions here instead of before calling these functions (?)
         if (PinDMsApi.isPinned(channelId)) return;
         const category = storage.pinnedDMs.findIndex(c => c.id === categoryId);
         storage.pinnedDMs[category].ids = [...storage.pinnedDMs[category].ids, channelId];
     },
-    unpin: (channelId) => {
+    unpin: (channelId: string): void => {
         const category = storage.pinnedDMs.findIndex(c => c.ids.includes(channelId));
         storage.pinnedDMs[category] = { ...storage.pinnedDMs[category], ids: storage.pinnedDMs[category].ids.filter(id => id != channelId) };
     },
-    isPinned: (channelId) => {
+    isPinned: (channelId: string): boolean => {
         return storage.pinnedDMs.some(dm => dm.ids.includes(channelId));
     },
-    getPinnedDMs: () => storage.pinnedDMs,
-    createCategory: (name) => {
+    getPinnedDMs: (): PinnedDMsCategory[] => storage.pinnedDMs,
+    createCategory: (name: string): void => {
         storage.pinnedDMs.push({
             ids: [],
             id: fromTimestamp(+Date.now()), // Generate random snowflake for category ID
             name
         })
     },
-    deleteCategory: (id) => {
+    deleteCategory: (id: string): void => {
         storage.pinnedDMs = storage.pinnedDMs.filter(category => category.id != id);
     }
 };
